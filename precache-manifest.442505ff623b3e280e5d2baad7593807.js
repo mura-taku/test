7dab8a7a41fba080351129a7a2993df0188b68c6
mura-taku <murakami.1426@icloud.com>
2020-07-29 15:11:08 +0900

    Updates

---
+++ precache-manifest.442505ff623b3e280e5d2baad7593807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356b548bc8b10242b23ae55c4342b621",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ea3f119615805dd4b5dc",
+    "url": "/test/static/css/main.f7c4cbd0.chunk.css"
+  },
+  {
+    "revision": "9fc1f450f04ed51a2375",
+    "url": "/test/static/js/2.c77618f9.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/test/static/js/2.c77618f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea3f119615805dd4b5dc",
+    "url": "/test/static/js/main.5a978af0.chunk.js"
+  },
+  {
+    "revision": "33626d860ddef518e844",
+    "url": "/test/static/js/runtime-main.74f173aa.js"
+  }
+]);
